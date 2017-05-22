414ea88da904f1f5768c987b18f460760b070234
Joel Maxuel <j.maxuel@gmail.com>
2017-05-21 21:52:05 -0300

    Function (Bash/fish): cdb - Change Directory Below
    Inspired by the DOS tool Directory Maven (created by Kent Briggs), cdb
    is a simple (non-indexing) replica that issues a real-time find to get
    the intended directory based on search term and then change to it.
    
    Due to the real-time nature, an entire filesystem would be too resource
    intensive to return a timely search (my home PC stores over two million
    directories), so the compromise is to only search below the current
    working directory (CWD).  Also, hidden directories (beginning with a
    dot) are excluded in the find process.
    
    After the initial concept was created (with a shim app made with Pascal),
    with help the Bash version was simplified, and then I attempted to to the
    same with the fish version.  Reaching out again I was given an example to
    fill in the gaps.  As a result the function looks a bit different based
    on which shell is being used.  Maybe I will clean that up later on.
    
    Special thanks to:
    Oliver Doepner (odoepner)
    Kurtis Rader (krader1961)

--- Bash/.bash_functions
+++ Bash/.bash_functions
@@ -1,3 +1,10 @@
 # ~/.bash_functions file, to store functions
 # created for the cli-productivity-config.
 
+# Change Directory Below
+function cdb() {
+  select dir in $(find -type d -name "$1" -not -path '*/\.*' -prune);
+  do
+    cd "${dir}" && break;
+  done 
+}
---
+++ fish/functions/cdb.fish
@@ -0,0 +1,58 @@
+function cdb --description 'Change Directory Below command'
+	set -l all_dirs (find -type d -name "$argv[1]" -not -path '*/\.*' -prune)
+    if not set -q all_dirs[1]
+        echo (_ 'No matches found.')
+        return 0
+    end
+
+    set -l letters a b c d e f g h i j k l m n o p q r s t u v w x y z
+    set -l dirc (count $all_dirs)
+    if test $dirc -gt (count $letters)
+        set -l msg (_ 'Too many results.  Truncating.')
+        printf "$msg\n"
+        set -l msg (_ 'There are %s unique dirs in your results but I can only handle %s')
+        printf "$msg\n" $dirc (count $letters)
+        set dirc (count $letters)
+    end
+
+	# Automatically go to directory if only one result
+    if test $dirc -eq 1
+		cd $all_dirs[1]
+        return 0
+    end
+	
+    # Print the matching directories.
+    for i in (seq 1 $dirc)
+        set -l dir $all_dirs[$i]
+        set -l label_color normal
+        set -q fish_color_cwd; and set label_color $fish_color_cwd
+        set -l dir_color_reset (set_color normal)
+        set -l dir_color
+
+        printf '%s %s %2d) %s %s%s%s\n' (set_color $label_color) $letters[$i] $i (set_color normal) $dir_color $dir $dir_color_reset
+    end
+
+    # Ask the user which directory from their results they want to cd to.
+    set -l msg (_ 'Select directory by letter or number: ')
+    read -l -p "echo '$msg'" choice
+    if test "$choice" = ""
+        return 0
+    else if string match -q -r '^[a-z]$' $choice
+        # Convert the letter to an index number.
+        set choice (contains -i $choice $letters)
+    end
+
+    set -l msg (_ 'Error: expected a number between 1 and %d or letter in that range, got "%s"')
+    if string match -q -r '^\d+$' $choice
+        if test $choice -ge 1 -a $choice -le $dirc
+            cd $all_dirs[$choice]
+            return
+        else
+            printf "$msg\n" $dirc $choice
+            return 1
+        end
+    else
+        printf "$msg\n" $dirc $choice
+        return 1
+    end
+end
